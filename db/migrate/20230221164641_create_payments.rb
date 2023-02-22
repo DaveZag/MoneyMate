c1bea1fd1e4f87478e0bff5ddc3fb5279394d812
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-02-22 16:52:43 +0200

    Add migrations and migrate database

--- db/migrate/20230221164641_create_payments.rb
+++ db/migrate/20230221164641_create_payments.rb
@@ -3,7 +3,9 @@ class CreatePayments < ActiveRecord::Migration[7.0]
     create_table :payments do |t|
       t.string :name
       t.integer :amount
-
+      t.references :author, null: false, foreign_key: { to_table: :users}
+      t.references :group, null: false
+      
       t.timestamps
     end
   end
