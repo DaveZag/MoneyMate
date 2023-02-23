0bb97c87d870f2ec640459553dbd02e1d0d6b96e
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-02-23 15:28:32 +0200

    Add tailwind Css
    Add devise views
    
    Add splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "home#index", as: :unauthenticated_user
+
+  authenticated do
+    root to: "groups#index", as: :authenticated_user
+  end
+
+  resources :payments, only: [:new, :create] 
+  resources :groups, only: [:index, :show, :new, :create]
 end
