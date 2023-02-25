80b267dc1ff766cc4c7cdfe727f68c44ce89d568
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-02-25 12:46:18 +0200

    Add partials for payment
    Update paths in controllers and views

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -11,13 +11,13 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    @payment = Transaction.new(payment_params)
+    @payment = Payment.new(payment_params)
 
     respond_to do |format|
       format.html do
         if @payment.save
-          flash[:sucess] = 'Transaction Saved Successfully'
-          redirect_to categories_path
+          flash[:notice] = 'Transaction Saved Successfully'
+          redirect_to group_path(id: payment_params['group_id'])
         else
           flash.now[:error] = 'Error: The New Transaction could not be saved'
           render :new, locals: { payment: @payment }
@@ -29,6 +29,6 @@ class PaymentsController < ApplicationController
   private
 
   def payment_params
-    params.require(:payment).permit(:name, :amount, :category_id).merge(author: current_user)
+    params.require(:payment).permit(:name, :amount, :group_id).merge(author: current_user)
   end
 end
