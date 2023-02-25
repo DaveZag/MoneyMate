b9907a9448e14bb0b0e1bf834e5bb1d4ee5e08d0
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-02-25 15:18:55 +0200

    Remove page caching to prevent javascript events from being lost
    Adjust views styling
    
    Add popup for icon input field

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,39 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+/* 
+    This script is for the icons popup.
+    Here I: 
+        - add event listeners to the popup's open and close buttons
+        - Retrieve the class of each icon e.g:  'arrow-fs fb' which will be used as a refecence in the Database.
+*/
+
+if (window.location.toString().includes("/groups/new")) {
+  let openBtn = document.querySelector(".select-icon");
+  openBtn.addEventListener("click", () => {
+    let popupSection = document.querySelector(".icon-container ");
+    if (popupSection.classList.contains("hidden")) {
+      popupSection.classList.remove("hidden");
+    }
+  });
+
+  let closeBtn = document.querySelector(".popup-close-btn");
+  closeBtn.addEventListener("click", () => {
+    let popupSection = document.querySelector(".icon-container ");
+    if (!popupSection.classList.contains("hidden")) {
+      popupSection.classList.add("hidden");
+    }
+  });
+
+  let icons = Array.from(document.querySelectorAll(".icon"));
+  icons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      let iconInput = document.querySelector(".icon-input");
+      let iconElement = icon.querySelector("i");
+      iconInput.value = Array.from(iconElement.classList).join(" ");
+      let popupSection = document.querySelector(".icon-container");
+      popupSection.classList.add("hidden");
+    });
+  });
+}
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
+import { Turbo } from "@hotwired/turbo-rails";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+Turbo.session.drive = false;
+
+export { application };
